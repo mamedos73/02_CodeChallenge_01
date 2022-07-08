68fed3ffe5171ca665297c3b4bcccf527c1fa3c2
mamedos73 <72689041+mamedos73@users.noreply.github.com>
2022-07-08 13:06:13 +0200

    Variante

--- challenge_01.js
+++ challenge_01.js
@@ -15,3 +15,52 @@ let zahl2 = parseInt(prompt("Geben Sie bitte Zahl 2"));
 let summeZahlen = zahl1 + zahl2;
 
 console.log("Die Summe der Zahlen ist: " + summeZahlen);
+
+/*********** Variante 1  ****************/
+// Vorteil: gute Lesbarkeit | aber viele Variablen
+
+// Deklaration
+// let ziffer1, ziffer2;
+// let zahl1, zahl2;
+// let summe;
+
+// // Eingabe | Wertzuweisung :: Ziffern
+// ziffer1 = prompt("Zahl 1: ");
+// ziffer2 = prompt("Zahl 2: ");
+// console.log(typeof ziffer1);
+// console.log(typeof ziffer2);
+
+// // Typkonvertierung | Ziffer --> Zahl (string --> number)
+// zahl1 = parseInt(ziffer1);
+// zahl2 = parseInt(ziffer2);
+// console.log(typeof zahl1);
+// console.log(typeof zahl2);
+
+// Berechnung :: Summe / Addition
+// summe = zahl1 + zahl2;
+
+// Ausgabe
+// console.log("Die Summe der Zahlen ist: " + summe);
+
+/*********** Variante 2  ****************/
+
+// Kompaktere Lösung
+
+// let zahl1, zahl2, summe;
+
+// zahl1 = parseInt(prompt("Zahl 1: "));
+// zahl2 = parseInt(prompt("Zahl 2: "));
+// summe = zahl1 + zahl2;
+// console.log("Die Summe der Zahlen ist: " + summe);
+
+/*********** Variante 3  ****************/
+// Kompakt, aber schlechter lesbar, unflexibel
+
+console.log
+(
+    "Die Summe ist: " + 
+   (
+    parseFloat(prompt("Bitte Zahl 1 eingeben")) +
+    parseFloat(prompt("Bitte Zahl 2 eingeben"))
+   ) 
+);
