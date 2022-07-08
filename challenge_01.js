2e4d679f392704fec169855808d8adb296dd4395
Roman <roman.makhanbetov@gmx.de>
2022-07-08 13:04:06 +0200

    My variant

--- challenge_01.js
+++ challenge_01.js
@@ -9,3 +9,9 @@ Geben Sie die Summe der Zahlen in die Konsole aus:
 "Die Summe der Zahlen ist: summe"
 */
 
+let zahl1 = parseInt(prompt("Geben Sie bitte Zahl 1"));
+let zahl2 = parseInt(prompt("Geben Sie bitte Zahl 2"));
+
+let summeZahlen = zahl1 + zahl2;
+
+console.log("Die Summe der Zahlen ist: " + summeZahlen);
